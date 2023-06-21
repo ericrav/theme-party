c2b58c03972f2f8311af3071abc97ebcee696478
Eric Rabinowitz <ericrabinowitz@me.com>
2023-06-21 18:22:19 -0400

    extract makeThemeProxy

--- src/ThemeParty.ts
+++ src/ThemeParty.ts
@@ -1,51 +1,22 @@
-import merge from "lodash/merge";
+import merge from 'lodash/merge';
 
-export type ThemeStyleValue = string | number | undefined;
-export type ResponseThemeValue<Breakpoints> = {
-  [K in keyof Breakpoints]?: ThemeStyleValue;
-};
-export type ThemeValue = ThemeStyleValue | ResponseThemeValue<any>;
-
-interface ThemeObject {
-  [key: string]: ThemeValue | ThemeObject;
-}
+import { makeThemeProxy } from './makeThemeProxy';
 
 interface ThemeConfig<Theme> {
   [key: string]: ((theme: Theme) => unknown) | ThemeConfig<Theme>;
 }
 
-function makeProxy<T extends ThemeObject>(theme: T): T {
-  const handler: ProxyHandler<any> = {
-    get(target, prop, receiver) {
-      console.log({ target, prop, receiver })
-      const value = Reflect.get(target, prop);
-
-      if (typeof value === "function") {
-        return value(new Proxy(theme, handler));
-      }
-
-      if (typeof value === "object" && value !== null) {
-        return new Proxy(value, handler);
-      }
-
-      return value;
-    }
-  }
-
-  return new Proxy(theme, handler);
-}
-
 export class ThemeParty<Theme extends {}> {
   public constructor(private theme: Theme) {}
 
-  #theme!: Theme
+  #theme!: Theme;
 
-  getTheme(): Theme {
-    this.#theme ??= makeProxy(this.theme);
+  public getTheme(): Theme {
+    this.#theme ??= makeThemeProxy(this.theme);
     return this.#theme;
   }
 
-  extend<T>(overrides: T & Partial<ThemeConfig<Theme>>) {
+  public extend<T>(overrides: T & Partial<ThemeConfig<Theme>>) {
     return new ThemeParty<Theme & T>(merge({}, this.theme, overrides));
   }
 }
---
+++ src/makeThemeProxy.ts
@@ -0,0 +1,19 @@
+export function makeThemeProxy<T>(theme: T): T {
+  const handler: ProxyHandler<any> = {
+    get(target, prop) {
+      const value = Reflect.get(target, prop);
+
+      if (typeof value === "function") {
+        return value(new Proxy(theme, handler));
+      }
+
+      if (typeof value === "object" && value !== null) {
+        return new Proxy(value, handler);
+      }
+
+      return value;
+    }
+  };
+
+  return new Proxy(theme, handler);
+}
