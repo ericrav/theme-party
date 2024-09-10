d2459a59107beea912bf6d76ec8beb3cb1a8a65e
Eric Rabinowitz <eric.rabinowitz@nytimes.com>
2024-09-10 14:43:24 -0400

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { JestConfigWithTsJest } from 'ts-jest'
 const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  testMatch: ['<rootDir>/src/**/*.test.ts?(x)'],
 };
 
 export default jestConfig;
