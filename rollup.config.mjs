a86b5b6293491f09ab180ff8310cd6d4c874c458
Eric Rabinowitz <eric.rabinowitz@nytimes.com>
2024-09-11 17:49:57 -0400

    add separate build tsconfig

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -29,7 +29,7 @@ export default [
     plugins: [
       resolve(),
       commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
+      typescript({ tsconfig: './tsconfig.build.json' }),
     ],
   },
   {
